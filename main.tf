0dd8b6d628e29efd4480e73cbaa730d3612d7a0e
serenaliqing <88020835+serenaliqing@users.noreply.github.com>
2025-02-05 21:20:03 +0800

    Add LA Consumption Deployment

---
+++ main.tf
@@ -0,0 +1,43 @@
+# Configure the Azure provider
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "workflow-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_logic_app_workflow" "example" {
+  name                = "workflow1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
